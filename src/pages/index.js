96eb50cf458f0b47a9e760435f91419479591506
Amy Boudsady <aeimskei@gmail.com>
2018-08-30 11:20:02 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello There!</h1>
+    <h2>This is my first Gatsby site.</h2>
+    <p>My name is Amy Boudsady and this was a pretty awesome and intuitibe process to get this up and running. Thank you freeCodeCamp and Netlify for making coding fun! Happy coding!!!</p>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
+  </div>
+)
+
+export default IndexPage
